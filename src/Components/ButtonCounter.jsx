b18710c5596050695d27932a1ece623b2c3dbbfd
Rickey Johnson <mr.rixter@gmail.com>
2022-09-06 00:01:49 +0000

    first commit

---
+++ src/Components/ButtonCounter.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+
+const ButtonCounter = () => {
+    const [count, setCount] = useState(0)
+    useEffect(() => {
+      document.title = `You clicked ${count} times`
+    });
+      
+  return (
+    <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+            Click Me
+        </button>
+    </div>
+  );
+}
+
+export default ButtonCounter;
